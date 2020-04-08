0b0bcf0e23edb074c6d0d19c8a7ab65e260d264d
goodluckparis <52003648+goodluckparis@users.noreply.github.com>
2020-04-08 17:26:57 +0200

    第五章

--- def-函数简介.py
+++ def-函数简介.py
@@ -5,25 +5,39 @@
 # print('再见')
 
 # 定义一个函数
-def fn():
-    print('这是我的第一个函数')
-    print('hello')
-    print('今天天气真不错')
+# def fn():
+#     print('这是我的第一个函数')
+#     print('hello')
+#     print('今天天气真不错')
 
 # 打印fn
 # print(fn) # <function fn at 0x10bc75430>
-print(type(fn)) # <class 'function'>
-fn()
-# 这是我的第一个函数
-fn()
-fn()
-fn()
+# print(type(fn)) # <class 'function'>
+# fn()
+# # 这是我的第一个函数
+# fn()
+# fn()
+# fn()
 # 这是我的第一个函数
 # 这是我的第一个函数
 # 这是我的第一个函数
 
+# fn是函数对象 fn()是调用函数
+# print是函数对象 print()是调用函数
+# fn() #这里候函数才会执行
 
+# 定义一个函数，可以用来求任意两个数的和
 
+# def sum() :
+#     a = 1
+#     b = 1
+#     print(a + b)
+#
+# sum()
+
+def fn2(a , b) : # a,b,c 就是形参
+    print('a =',a)
+    print('b =',b)
 
 
 
